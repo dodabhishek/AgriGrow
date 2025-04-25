ce1335699b9a2c509eb60a22619621e2f0ecb2f1
Abhishek Thakur <76610164+dodabhishek@users.noreply.github.com>
2025-04-25 11:10:05 +0530

    added the login page and profile page for right now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,17 @@ import Service from './pages/Service.jsx';
 import Contact from "./pages/Contact.jsx";
 import Projects from "./pages/Projects.jsx";
 import About from './pages/About.jsx'
-import Shop from "./pages/Shop.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import { useAuthStore } from "./store/useAuthStore.js";
+import { Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
  // Import your Service page
 
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
   return (
     <Router>
       <Header />
@@ -22,6 +28,9 @@ const App = () => {
         <Route path="/service" element={<Service />} /> {/* Service Route */}
         <Route path="/projects" element={<Projects />} /> {/* Project Route */}
         <Route path="/shop" element={<Shop />} /> {/* Project Route */}
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         
         <Route path="/contact" element={<Contact />} /> {/* Contact Route */}
 
@@ -29,9 +38,10 @@ const App = () => {
         
 
       </Routes>
-
+      <Toaster />
       <Footer />
     </Router>
+    
   );
 };
 
