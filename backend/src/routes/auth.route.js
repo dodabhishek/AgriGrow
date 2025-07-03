574b881457f6ead5e521cdbe76b2e1e84732a46b
Abhishek Thakur <76610164+dodabhishek@users.noreply.github.com>
2025-07-03 14:33:48 +0530

    fixed the regex error

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -9,5 +9,4 @@ router.post("/login", login);
 router.post("/logout", logout);
 router.put("/update-profile", protectRoute, updateProfile);
 router.get("/check", protectRoute, checkAuth);
-router.post("/signup", adminProtect,login);
 export default router;
