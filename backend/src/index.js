14c83cb097d76540757cdc9a343385e538ea6ecf
Abhishek Thakur <76610164+dodabhishek@users.noreply.github.com>
2025-07-03 15:39:44 +0530

    asd

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,8 +38,8 @@ app.use('/api/cart', cartRoutes);
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    app.get('/*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
     });
   }
 
