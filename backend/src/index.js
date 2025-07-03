3aeb9ef2a4b97bb10969c551ca124fb92508e51d
Abhishek Thakur <76610164+dodabhishek@users.noreply.github.com>
2025-07-03 16:33:55 +0530

    added splat

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,52 +1,51 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import { connectDB } from './lib/db.js';
+import { app, server } from './lib/socket.js';
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/productRoute.js';
 import cartRoutes from './routes/cartRoute.js';
 import messageRoutes from './routes/message.route.js';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { app, server } from './lib/socket.js';
-import path from 'path';
-
 
 dotenv.config();
 
-// Get the directory path for ES modules
-const __dirname = path.resolve();
+// __dirname workaround for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
 
+// Middlewares
 app.use(cors({
-    origin: true,
-    credentials: true,
+  origin: true,
+  credentials: true,
 }));
-
-// Middleware
 app.use(cookieParser());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-// API Routes
+// Routes
+console.log('✅ Mounting routes...');
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 
-// Serve static files from the React app in production
-
+// Serve frontend
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get('/*)', (req, res) => {
+    app.get('/{*splat}', (req, res) => {
       res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
     });
   }
 
-
-
-// Start the server
+// Start server
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
+  console.log(`🚀 Server running on port ${PORT}`);
+  connectDB();
 });
